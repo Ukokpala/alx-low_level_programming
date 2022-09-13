f567189f03fa123326207dd9e29cebdaacb94a88
Ukokpala <okpalaukamka480@gmail.com>
2022-09-13 07:55:50 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * main - prints _putchar, followed by a new line
- * Return: Always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n')
+char *sh = "Holberton";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
 return (0);
 }
