91fda6914b8d12fab4a8b11d14f9e99926c75fe4
Ukokpala <okpalaukamka480@gmail.com>
2022-09-15 12:43:50 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,60 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-* print_number - Prints a number
-* @n: The number to print
-*/
-
+ * print_number - prints a number using _putchar
+ * @n: number to print
+ *
+ * Return: void
+ */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int un = 0;
 
 	if (n < 0)
 	{
-		putchar('-');
-		num = -num;
+		if (n < 1000000000)
+			n = -n;
+		un = n;
+		_putchar(45);
+		num_to_char(n);
 	}
-	if (num > 9)
+	else
 	{
-		print_number(num / 10);
+		un = n;
+		num_to_char(un);
 	}
-	putchar(num % 10 + '0');
+}
+
+/**
+ * num_to_char - transforms a number with 1 or more digits into a char
+ * @n: number to print
+ *
+ * Return: void
+ */
+void num_to_char(unsigned int n)
+{
+	unsigned int d = 10;
+
+	if (n < d)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		while (n >= d)
+		{
+			d *= 10;
+			if (d == 1000000000)
+				break;
+		}
+		if (!(d == 1000000000) || n == 123456789)
+			d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
+	}
 }
