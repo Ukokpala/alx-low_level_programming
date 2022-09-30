c625e3d24e600f876ca0214029a0d19e31b4605b
Ukokpala <okpalaukamka480@gmail.com>
2022-09-30 11:55:46 -0700

    commit 0 and 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
-#include <unistd.h>                                                                                                                  
-                                                                                                                                     
-/**                                                                                                                                  
- * _putchar - writes the character c to stdout                                                                                       
- * @c: The character to print                                                                                                        
- *                                                                                                                                   
- * Return: On success 1.                                                                                                             
- * On error, -1 is returned, and errno is set appropriately.                                                                         
- */                                                                                                                                  
-int _putchar(char c)                                                                                                                 
-{                                                                                                                                    
-return (write(1, &c, 1));                                                                                                            
-}
+#include <stdio.h>
 
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
